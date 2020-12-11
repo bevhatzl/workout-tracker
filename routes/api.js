fd111b22e8b50acfffd651ea0a338310e7c498b6
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-12-11 23:53:21 +1100

    dashboard working

--- routes/api.js
+++ routes/api.js
@@ -1,90 +1,7 @@
-// const router = require("express").Router();
 const path = require("path");
-// const Workout = require("../models");
 let db = require("../models");
 
 module.exports = function (app) {
-
-    // New workout, creates an id (index)
-    // app.post("/api/workouts", ({ body }, res) => {
-    //     Workout.create(body)
-    //         .then(dbWorkout => {
-    //             res.json(dbWorkout);
-    //         })
-    //         .catch(err => {
-    //             res.status(400).json(err);
-    //         });
-    // });
-
-    // // Add exercise, updates an existing workout
-    // app.put("/api/workouts/:id", (req, res) => {
-    //     Workout.findOneAndUpdate(
-    //         { _id: req.params.id },
-    //         {
-    //             $inc: { totalDuration: req.body.duration },
-    //             $push: { exercises: req.body }
-    //         },
-    //         { new: true }).then(dbWorkout => {
-    //             res.json(dbWorkout);
-    //         }).catch(err => {
-    //             res.json(err);
-    //         });
-    //     // Workout.updateOne({ _id: req.params.id },
-    //     //     {
-    //     //         $inc: { totalDuration: req.body.duration },
-    //     //         $push: { exercises: req.body }
-    //     //         // Another way to add by object literal
-    //     //         // $push: {
-    //     //         //     exercises: {
-    //     //         //          type: req.body.type,
-    //     //         //          name: req.body.name
-    //     //         //     }
-    //     //         // }
-    //     //     })
-    //     //     .then(dbWorkout => {
-    //     //         res.json(dbWorkout);
-    //     //     })
-    //     //     .catch(err => {
-    //     //         res.status(400).json(err);
-    //     //     });
-    // });
-
-    // // Get all workouts
-    // app.get("/api/workouts", (req, res) => {
-    //     Workout.find({}).then(dbWorkout => {
-    //         dbWorkout.forEach(workout => {
-    //             let total = 0;
-    //             workout.exercises.forEach(e => {
-    //                 total += e.duration;
-    //             });
-    //             workout.totalDuration = total;
-    //         });
-    //         res.json(dbWorkout);
-    //     }).catch(err => {
-    //         res.json(err);
-    //     });
-    //     // Workout.find({})
-    //     //     .sort({ date: -1 })
-    //     //     .then(dbWorkout => {
-    //     //         res.json(dbWorkout);
-    //     //     })
-    //     //     .catch(err => {
-    //     //         res.status(400).json(err);
-    //     //     });
-    // });
-
-    // app.get("/api/workouts/range", (req, res) => {
-    //     Workout.find({}).then(dbWorkout => {
-    //         res.json(dbWorkout);
-    //     }).catch(err => {
-    //         res.json(err);
-    //     });
-    // });
-
-
-
-
-
     // Used by api.js to get last workout
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
@@ -96,7 +13,7 @@ module.exports = function (app) {
             });
     });
 
-    // Creates a new workout in the workout database
+    // Creates a new workout in the database
     app.post("/api/workouts", async (req, res) => {
         try {
             const response = await db.Workout.create({ type: "workout" })
@@ -107,16 +24,14 @@ module.exports = function (app) {
         }
     })
 
-    // Used by api.js to add an exercise to a workout
+    // Used by api.js to add an exercise to an existing workout
     app.put("/api/workouts/:id", ({ body, params }, res) => {
-        // console.log(body, params)
         const workoutId = params.id;
         let savedExercises = [];
 
         // gets all the currently saved exercises in the current workout
         db.Workout.find({ _id: workoutId })
             .then(dbWorkout => {
-                // console.log(dbWorkout)
                 savedExercises = dbWorkout[0].exercises;
                 res.json(dbWorkout[0].exercises);
                 let allExercises = [...savedExercises, body]
@@ -138,6 +53,7 @@ module.exports = function (app) {
 
     })
 
+    // Used by the stats page
     app.get("/api/workouts/range", (req, res) => {
         db.Workout.find({})
             .then(workout => {
@@ -149,11 +65,12 @@ module.exports = function (app) {
     });
 
 
-    // route for the stats page
+    // HTML route for the stats page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
+    // HTML route for continue exercise
     app.get("/exercise", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
@@ -163,4 +80,3 @@ module.exports = function (app) {
     });
 
 }
-// module.exports = router;
