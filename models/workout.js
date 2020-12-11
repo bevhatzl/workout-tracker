17cc050a5f720b533a70ca103f9dc81f03f53782
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-12-11 21:45:15 +1100

    link to stats working

--- models/workout.js
+++ models/workout.js
@@ -4,36 +4,58 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     day: {
-        type: Date
+        type: Date,
+        default: Date.now
     },
     exercises: [{
         type: {
             type: String,
-            trim: true,
-            required: "Select the excercise type"
+            trim: true
+            // required: "Select the excercise type"
         },
         name: {
             type: String,
-            trim: true,
-            required: "Enter the excercise name"
+            trim: true
+            // required: "Enter the excercise name"
         },
         duration: {
-            type: Number,
-            required: false
+            type: Number
+            // required: false
         },
         weight: {
             type: Number,
-            required: false
+            default: 0
+            // required: false
         },
         reps: {
             type: Number,
-            required: false
+            default: 0
+            // required: false
         },
         sets: {
             type: Number,
-            required: false
+            default: 0
+            // required: false
+        },
+        distance: {
+            type: Number
         }
     }]
+},
+    {
+        toJSON: {
+            // include any virtual properties when data is requested
+            virtuals: true
+        }
+    }
+);
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+const path = require("path");
 const Workout = require("../models/workout.js");
 
-// Complete excercise
+// New workout, creates an id (index)
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
@@ -12,36 +13,77 @@ router.post("/api/workouts", ({ body }, res) => {
         });
 });
 
-// Add excercise
+// Add exercise, updates an existing workout
 router.put("/api/workouts/:id", (req, res) => {
-    Workout.updateOne({ _id: req.params.id }, {
-        $push: { exercises: req.body } // object deconstruction
-        // Another way to add by object literal
-        // $push: {
-        //     exercises: {
-        //     //     // type: req.body.type,
-        //     //     // name: req.body.name
-        //     }
-        // }
-    })
-        .then(dbWorkout => {
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
             res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
+        }).catch(err => {
+            res.json(err);
         });
+    // Workout.updateOne({ _id: req.params.id },
+    //     {
+    //         $inc: { totalDuration: req.body.duration },
+    //         $push: { exercises: req.body }
+    //         // Another way to add by object literal
+    //         // $push: {
+    //         //     exercises: {
+    //         //          type: req.body.type,
+    //         //          name: req.body.name
+    //         //     }
+    //         // }
+    //     })
+    //     .then(dbWorkout => {
+    //         res.json(dbWorkout);
+    //     })
+    //     .catch(err => {
+    //         res.status(400).json(err);
+    //     });
 });
 
 // Get all workouts
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-        .sort({ date: -1 })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
+    Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            let total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
         });
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+    // Workout.find({})
+    //     .sort({ date: -1 })
+    //     .then(dbWorkout => {
+    //         res.json(dbWorkout);
+    //     })
+    //     .catch(err => {
+    //         res.status(400).json(err);
+    //     });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        console.log("ALL WORKOUTS");
+        console.log(dbWorkout);
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+// route for the stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
